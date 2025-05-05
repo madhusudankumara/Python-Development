0a8a99eb3e98a48d59b9f244c8a3e74901e56944
madhusudan kumar <madhusudanshambhu@gmail.com>
2025-05-05 21:10:01 +0530

    python learning

--- firstProgram.py
+++ firstProgram.py
@@ -1,6 +1,15 @@
-print("hellow World","i am madhusudan")
-name= "madhusudan"
-age=27
-price = 22.39
-print("my name is : ",name)
-print("my age is : ",age)
+# print("hellow World","i am madhusudan")
+# name= "madhusudan"
+# age=27
+# price = 22.39
+# print("my name is : ",name)
+# print("my age is : ",age)
+# print(not True)
+# print(not False)
+
+name = input("enter your name")
+print("welcome", name)
+
+first = int(input("enter first number"))
+second = int(input("enter second number"))
+print("sum is ", first+second)
